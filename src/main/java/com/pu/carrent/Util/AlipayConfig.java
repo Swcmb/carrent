9374397e46c477d18c524a87d0a26a143998f0ae
Swcmb <3216243013@qq.com>
2025-06-09 23:53:11 +0800

    批量提交：修复XX功能

---
+++ src/main/java/com/pu/carrent/Util/AlipayConfig.java
@@ -0,0 +1,20 @@
+package com.pu.carrent.Util;
+
+public class AlipayConfig {
+
+    public static String app_id = "2016100100637161";
+
+    public static String merchant_private_key = "";
+
+    public static String alipay_public_key = "";
+
+    public static String notify_url = "http://wikdjh.natappfree.cc/notify_url";
+
+    public static String return_url = "http://wikdjh.natappfree.cc/return_url";
+
+    public static String sign_type = "RSA2";
+
+    public static String charset = "gbk";
+
+    public static String gatewayUrl = "https://openapi.alipaydev.com/gateway.do";
+}
